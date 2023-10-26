ec7073ca06d32f6406f28cf34cf44c9d4fb34a57
TTTTy <1069232780@qq.com>
2023-10-26 12:46:02 +0800

    start

---
+++ src/view/Welcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="main">
+123123123
+    </div>
+</template>
+
+<style  scoped>
+.main {
+    text-align: center;
+    padding: 5px;
+    background: white;
+    height: 100%;
+    /* background-image: url("../assets/img/index.gif"); */
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-position-y: bottom;
+}
+</style>
